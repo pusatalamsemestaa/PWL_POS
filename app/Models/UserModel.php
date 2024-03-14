de5cdf072f8919cfee17edeb459d9570cecd783b
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2024-03-14 13:37:59 +0700

    user

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primarykey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
 
     protected $fillable = ['level_id', 'username', 'nama'];
 }
